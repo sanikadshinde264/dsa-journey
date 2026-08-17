74bf7c95865900b0616e83479af886887dc9dad5
Sanika Shinde <139208595+sanikadshinde264@users.noreply.github.com>
2026-08-17 12:59:22 +0530

    Create largest_number_from_array.java

---
+++ array/largest_number_from_array.java
@@ -0,0 +1,17 @@
+class Main 
+{
+    public static void main(String[] args) 
+    {
+        int arr[] = {2, 5, 1, 3, 0};
+        int largest=arr[0];
+
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>largest)
+            {
+                largest=arr[i];
+            }
+        }
+        System.out.println("Largest Number From Array:"+largest);
+    }
+}
